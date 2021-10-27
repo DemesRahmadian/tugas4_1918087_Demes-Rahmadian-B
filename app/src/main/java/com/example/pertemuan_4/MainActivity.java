5b8ac590438f3d58daa7033399618323aa1c352b
abdillahachmad <achmadabdillah20@gmail.com>
2021-10-28 00:25:04 +0700

    tugas4 1918107

--- app/src/main/java/com/example/pertemuan_4/MainActivity.java
+++ app/src/main/java/com/example/pertemuan_4/MainActivity.java
@@ -1,34 +1,43 @@
-package com.example.pertemuan_4;
+    package com.example.pertemuan_4;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.recyclerview.widget.LinearLayoutManager;
+    import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
+    import android.os.Bundle;
 
-import java.util.ArrayList;
+    import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private MahasiswaAdapter adapter;
-    private ArrayList<Mahasiswa> mahasiswaArrayList;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        addData();
-        recyclerView =
-                (RecyclerView)findViewById(R.id.recycleview);
-        adapter = new MahasiswaAdapter (mahasiswaArrayList);
-        RecyclerView.LayoutManager layoutManager = new
-                LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
+    public class MainActivity extends AppCompatActivity {
+        private RecyclerView recyclerView;
+        private MahasiswaAdapter adapter;
+        private ArrayList<Mahasiswa> mahasiswaArrayList;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            addData();
+
+            recyclerView = (RecyclerView)findViewById(R.id.recycleview);
+
+            adapter = new MahasiswaAdapter(mahasiswaArrayList);
+
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+            recyclerView.setLayoutManager(layoutManager);
+
+            recyclerView.setAdapter(adapter);
+
+        }
+
+        private void addData() {
+            mahasiswaArrayList = new ArrayList<>();
+            mahasiswaArrayList.add(new Mahasiswa(R.drawable.fotosmk,"Nur Achmad Abdillah ", "1918107", "2019"));
+            mahasiswaArrayList.add(new Mahasiswa(R.drawable.ic_launcher_background,"Budi", "1718082", "2017"));
+            mahasiswaArrayList.add(new Mahasiswa(R.drawable.fotosmk,"Mufid ", "1818002", "2018"));
+            mahasiswaArrayList.add(new Mahasiswa(R.drawable.ic_launcher_foreground,"Banteng", "1618008", "2016"));
+            mahasiswaArrayList.add(new Mahasiswa(R.drawable.fotosmk,"Aku", "1618039", "2016"));
+        }
     }
-    private void addData() {
-        mahasiswaArrayList = new ArrayList<>();
-        mahasiswaArrayList.add(new Mahasiswa("Agung", "1218019", "2012"));
-        mahasiswaArrayList.add(new Mahasiswa("Abi", "1118002", "2011"));
-        mahasiswaArrayList.add(new Mahasiswa("Yudis", "1118031", "2011"));
-    }
-}
